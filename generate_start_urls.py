7ec9822a64117206774742fdada176272f606ec9
Enrique Garcia <garcianavalon@gmail.com>
2014-05-20 15:54:57 +0800

    More cities

--- generate_start_urls.py
+++ generate_start_urls.py
@@ -1,7 +1,8 @@
 f = open("urls.txt", 'w')
-base_url = "http://www.tripadvisor.com/ShowForum-g186338-i17-London_England.html\n"
+f.truncate()
+base_url = "http://www.tripadvisor.com/ShowForum-g187147-i14-Paris_Ile_de_France.html\n"
 f.write(base_url)
-for i in range(20,93180,20):
-    new_url = "http://www.tripadvisor.com/ShowForum-g186338-i17-o"+str(i)+"-London_England.html\n"
+for i in range(20,99320,20):
+    new_url = "http://www.tripadvisor.com/ShowForum-g187147-i14-o"+str(i)+"-Paris_Ile_de_France.html\n"
     f.write(new_url)
 f.close()
