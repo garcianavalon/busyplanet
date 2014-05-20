2c5fb22f3b00ee9a42b4388908d2b5786e6da9d8
Enrique Garcia <garcianavalon@gmail.com>
2014-05-20 15:53:54 +0800

    fixed errors

--- word_count/extract_positive_words_count.py
+++ word_count/extract_positive_words_count.py
@@ -1,5 +1,6 @@
 f = open('positivewords.txt','r')
-lines = f.readlines()
+lines = [url.strip() for url in f.readlines()]
+print lines
 f.close()
 
 from pymongo import MongoClient
@@ -8,6 +9,9 @@ db = client['bigdata']
 
 cities = ['sanfrancisco']
 for city in cities:
+    all_words = db['word_count_'+city]
+    positive_words = db['positive_words_'+city]
     for word in lines:
-        item = db['word_count_'+city].find({'_id':word})
-        db['positive_words_'+city].insert(item)
+        item = all_words.find_one({'_id':word})
+        if item:
+            positive_words.insert(item)
