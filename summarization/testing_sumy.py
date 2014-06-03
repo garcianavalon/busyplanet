33bd41ef8b73e7504c2a03ae66b9633554d930bb
Enrique Garcia <garcianavalon@gmail.com>
2014-06-03 20:51:10 +0800

    adjectives

--- summarization/testing_sumy.py
+++ summarization/testing_sumy.py
@@ -1,5 +1,3 @@
-# -*- coding: utf8 -*-
-
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
@@ -8,17 +6,6 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
 from sumy.nlp.stemmers.czech import stem_word
 from sumy.utils import get_stop_words
-
-
-if __name__ == "__main__":
-    url = "http://www.zsstritezuct.estranky.cz/clanky/predmety/cteni/jak-naucit-dite-spravne-cist.html"
-    parser = HtmlParser.from_url(url, Tokenizer("czech"))
-
-    summarizer = LsaSummarizer(stem_word)
-    summarizer.stop_words = get_stop_words("czech")
-
-    for sentence in summarizer(parser.document, 20):
-        print(sentence)
 
 def summarize():
     pass #TODO
@@ -31,5 +18,20 @@ def getData(amount=-1):
     cities = ['sanfrancisco']
     data = []
     for city in cities:
-        pass
+        comments_colletion = db['tripadvisor_'+city]
+        for comment in comments_colletion.find().limit(10):
+            data.extend(comment['text'])
     return data
+if __name__ == "__main__":
+    url = "http://www.zsstritezuct.estranky.cz/clanky/predmety/cteni/jak-naucit-dite-spravne-cist.html"
+    parser = HtmlParser.from_url(url, Tokenizer("czech"))
+
+    summarizer = LsaSummarizer(stem_word)
+    summarizer.stop_words = get_stop_words("czech")
+
+    for sentence in summarizer(parser.document, 20):
+        #print(sentence)
+        pass
+    data = getData()
+    print(data)
+
---
+++ word_count/adjectives_extraction.py
@@ -0,0 +1,35 @@
+f = open('positivewords.txt','r')
+adjectives = [url.strip() for url in f.readlines()]
+f.close()
+
+from pymongo import MongoClient
+client = MongoClient()
+db = client['bigdata']
+
+def extractWords(index, list):
+    words = []
+    for i in [index-2,index-1,index,index+1,index+2]:
+        if i>0 and i<list.count-1:
+            words.append(list[i])
+    return words
+
+cities = ['sanfrancisco','london']
+for city in cities:
+    print 'Starting with: '+city
+    comments_colletion = db['tripadvisor_'+city] #change to the name of your collections
+    result_collection = db['result_'+city]
+    #clean start
+    result_collection.remove()
+    for comment in comments_colletion.find().limit(1000):#you can get less for testing
+        text = comment['text']
+        #add all the text
+        text_as_string = ""
+        for p in text:
+            text_as_string += p.replace(".","").lower()
+        text_as_words = text_as_string.split()
+        for adjective in adjectives:
+            if adjective in text_as_words:
+                index = text_as_words.index(adjective)
+                words = extractWords(index,text_as_words)
+                print(words)
+    print 'Finished with: '+city
